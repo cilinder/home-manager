38ee75d2078feb0e2a239bf196ffbc4afc2f8bfc
Robert Helgesson <robert@rycee.net>
2018-12-16 12:23:01 +0100

    modules: support conditional module inclusion

--- modules/modules.nix
+++ modules/modules.nix
@@ -12,106 +12,116 @@ with lib;
 
 let
 
-  modules = [
-    ./accounts/email.nix
-    ./files.nix
-    ./home-environment.nix
-    ./manual.nix
-    ./misc/fontconfig.nix
-    ./misc/gtk.nix
-    ./misc/lib.nix
-    ./misc/news.nix
-    ./misc/nixpkgs.nix
-    ./misc/pam.nix
-    ./misc/qt.nix
-    ./misc/version.nix
-    ./misc/xdg.nix
-    ./programs/afew.nix
-    ./programs/alot.nix
-    ./programs/astroid.nix
-    ./programs/autorandr.nix
-    ./programs/bash.nix
-    ./programs/beets.nix
-    ./programs/browserpass.nix
-    ./programs/command-not-found/command-not-found.nix
-    ./programs/direnv.nix
-    ./programs/eclipse.nix
-    ./programs/emacs.nix
-    ./programs/feh.nix
-    ./programs/firefox.nix
-    ./programs/fish.nix
-    ./programs/fzf.nix
-    ./programs/git.nix
-    ./programs/gnome-terminal.nix
-    ./programs/go.nix
-    ./programs/home-manager.nix
-    ./programs/htop.nix
-    ./programs/info.nix
-    ./programs/jq.nix
-    ./programs/lesspipe.nix
-    ./programs/man.nix
-    ./programs/mbsync.nix
-    ./programs/mercurial.nix
-    ./programs/msmtp.nix
-    ./programs/neovim.nix
-    ./programs/newsboat.nix
-    ./programs/noti.nix
-    ./programs/notmuch.nix
-    ./programs/obs-studio.nix
-    ./programs/offlineimap.nix
-    ./programs/pidgin.nix
-    ./programs/rofi.nix
-    ./programs/ssh.nix
-    ./programs/taskwarrior.nix
-    ./programs/termite.nix
-    ./programs/texlive.nix
-    ./programs/tmux.nix
-    ./programs/urxvt.nix
-    ./programs/vim.nix
-    ./programs/vscode.nix
-    ./programs/zathura.nix
-    ./programs/zsh.nix
-    ./services/blueman-applet.nix
-    ./services/compton.nix
-    ./services/dunst.nix
-    ./services/flameshot.nix
-    ./services/gnome-keyring.nix
-    ./services/gpg-agent.nix
-    ./services/kbfs.nix
-    ./services/kdeconnect.nix
-    ./services/keepassx.nix
-    ./services/keybase.nix
-    ./services/mbsync.nix
-    ./services/mpd.nix
-    ./services/network-manager-applet.nix
-    ./services/nextcloud-client.nix
-    ./services/owncloud-client.nix
-    ./services/parcellite.nix
-    ./services/pasystray.nix
-    ./services/polybar.nix
-    ./services/random-background.nix
-    ./services/redshift.nix
-    ./services/screen-locker.nix
-    ./services/stalonetray.nix
-    ./services/status-notifier-watcher.nix
-    ./services/syncthing.nix
-    ./services/taffybar.nix
-    ./services/tahoe-lafs.nix
-    ./services/udiskie.nix
-    ./services/unclutter.nix
-    ./services/window-managers/awesome.nix
-    ./services/window-managers/i3.nix
-    ./services/window-managers/xmonad.nix
-    ./services/xscreensaver.nix
-    ./systemd.nix
-    ./xcursor.nix
-    ./xresources.nix
-    ./xsession.nix
-    <nixpkgs/nixos/modules/misc/assertions.nix>
-    <nixpkgs/nixos/modules/misc/meta.nix>
+  hostPlatform = pkgs.stdenv.hostPlatform;
+
+  checkPlatform = any (meta.platformMatch pkgs.stdenv.hostPlatform);
+
+  loadModule = { file, condition ? true }: {
+    inherit file condition;
+  };
+
+  allModules = [
+    (loadModule { file = ./accounts/email.nix; })
+    (loadModule { file = ./files.nix; })
+    (loadModule { file = ./home-environment.nix; })
+    (loadModule { file = ./manual.nix; })
+    (loadModule { file = ./misc/fontconfig.nix; })
+    (loadModule { file = ./misc/gtk.nix; })
+    (loadModule { file = ./misc/lib.nix; })
+    (loadModule { file = ./misc/news.nix; })
+    (loadModule { file = ./misc/nixpkgs.nix; })
+    (loadModule { file = ./misc/pam.nix; })
+    (loadModule { file = ./misc/qt.nix; })
+    (loadModule { file = ./misc/version.nix; })
+    (loadModule { file = ./misc/xdg.nix; })
+    (loadModule { file = ./programs/afew.nix; })
+    (loadModule { file = ./programs/alot.nix; })
+    (loadModule { file = ./programs/astroid.nix; })
+    (loadModule { file = ./programs/autorandr.nix; })
+    (loadModule { file = ./programs/bash.nix; })
+    (loadModule { file = ./programs/beets.nix; })
+    (loadModule { file = ./programs/browserpass.nix; })
+    (loadModule { file = ./programs/command-not-found/command-not-found.nix; })
+    (loadModule { file = ./programs/direnv.nix; })
+    (loadModule { file = ./programs/eclipse.nix; })
+    (loadModule { file = ./programs/emacs.nix; })
+    (loadModule { file = ./programs/feh.nix; })
+    (loadModule { file = ./programs/firefox.nix; })
+    (loadModule { file = ./programs/fish.nix; })
+    (loadModule { file = ./programs/fzf.nix; })
+    (loadModule { file = ./programs/git.nix; })
+    (loadModule { file = ./programs/gnome-terminal.nix; })
+    (loadModule { file = ./programs/go.nix; })
+    (loadModule { file = ./programs/home-manager.nix; })
+    (loadModule { file = ./programs/htop.nix; })
+    (loadModule { file = ./programs/info.nix; })
+    (loadModule { file = ./programs/jq.nix; })
+    (loadModule { file = ./programs/lesspipe.nix; })
+    (loadModule { file = ./programs/man.nix; })
+    (loadModule { file = ./programs/mbsync.nix; })
+    (loadModule { file = ./programs/mercurial.nix; })
+    (loadModule { file = ./programs/msmtp.nix; })
+    (loadModule { file = ./programs/neovim.nix; })
+    (loadModule { file = ./programs/newsboat.nix; })
+    (loadModule { file = ./programs/noti.nix; })
+    (loadModule { file = ./programs/notmuch.nix; })
+    (loadModule { file = ./programs/obs-studio.nix; })
+    (loadModule { file = ./programs/offlineimap.nix; })
+    (loadModule { file = ./programs/pidgin.nix; })
+    (loadModule { file = ./programs/rofi.nix; })
+    (loadModule { file = ./programs/ssh.nix; })
+    (loadModule { file = ./programs/taskwarrior.nix; })
+    (loadModule { file = ./programs/termite.nix; })
+    (loadModule { file = ./programs/texlive.nix; })
+    (loadModule { file = ./programs/tmux.nix; })
+    (loadModule { file = ./programs/urxvt.nix; })
+    (loadModule { file = ./programs/vim.nix; })
+    (loadModule { file = ./programs/vscode.nix; })
+    (loadModule { file = ./programs/zathura.nix; })
+    (loadModule { file = ./programs/zsh.nix; })
+    (loadModule { file = ./services/blueman-applet.nix; })
+    (loadModule { file = ./services/compton.nix; })
+    (loadModule { file = ./services/dunst.nix; })
+    (loadModule { file = ./services/flameshot.nix; })
+    (loadModule { file = ./services/gnome-keyring.nix; })
+    (loadModule { file = ./services/gpg-agent.nix; })
+    (loadModule { file = ./services/kbfs.nix; })
+    (loadModule { file = ./services/kdeconnect.nix; })
+    (loadModule { file = ./services/keepassx.nix; })
+    (loadModule { file = ./services/keybase.nix; })
+    (loadModule { file = ./services/mbsync.nix; })
+    (loadModule { file = ./services/mpd.nix; })
+    (loadModule { file = ./services/network-manager-applet.nix; })
+    (loadModule { file = ./services/nextcloud-client.nix; })
+    (loadModule { file = ./services/owncloud-client.nix; })
+    (loadModule { file = ./services/parcellite.nix; })
+    (loadModule { file = ./services/pasystray.nix; })
+    (loadModule { file = ./services/polybar.nix; })
+    (loadModule { file = ./services/random-background.nix; })
+    (loadModule { file = ./services/redshift.nix; })
+    (loadModule { file = ./services/screen-locker.nix; })
+    (loadModule { file = ./services/stalonetray.nix; })
+    (loadModule { file = ./services/status-notifier-watcher.nix; })
+    (loadModule { file = ./services/syncthing.nix; })
+    (loadModule { file = ./services/taffybar.nix; })
+    (loadModule { file = ./services/tahoe-lafs.nix; })
+    (loadModule { file = ./services/udiskie.nix; })
+    (loadModule { file = ./services/unclutter.nix; })
+    (loadModule { file = ./services/window-managers/awesome.nix; })
+    (loadModule { file = ./services/window-managers/i3.nix; })
+    (loadModule { file = ./services/window-managers/xmonad.nix; })
+    (loadModule { file = ./services/xscreensaver.nix; })
+    (loadModule { file = ./systemd.nix; })
+    (loadModule { file = ./xcursor.nix; })
+    (loadModule { file = ./xresources.nix; })
+    (loadModule { file = ./xsession.nix; })
+    (loadModule { file = <nixpkgs/nixos/modules/misc/assertions.nix>; })
+    (loadModule { file = <nixpkgs/nixos/modules/misc/meta.nix>; })
   ]
   ++
-  optional pkgs.stdenv.isLinux ./programs/chromium.nix;
+  optional pkgs.stdenv.isLinux (loadModule { file = ./programs/chromium.nix; });
+
+  modules = map (getAttr "file") (filter (getAttr "condition") allModules);
 
   pkgsModule = {
     options.nixosSubmodule = mkOption {
